02f85bf0fa7f7415f83be1229a794849b70c8c7d
All4n <86030627+ItsAll4n@users.noreply.github.com>
2023-11-07 00:30:41 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import json, subprocess, autoit, win32gui, pytesseract, pyautogui
+import json, subprocess, win32gui, pytesseract, pyautogui, win32com.client, os, time
 from PIL import Image
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+autoit = win32com.client.Dispatch("AutoItX3.Control")
+
 pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+
+os.system(f'title AutoRaid ^')
 
 with open("config.json", "r") as config:
     config = json.load(config)
 
+sleep_timer = config['wait_time']
 check_hog = config['check_hog']
 check_sb = config['check_sb']
 webhook_enabled = config['webhook_enabled']
@@ -17,16 +22,17 @@ if webhook_enabled == True:
     webhook = DiscordWebhook(url=webhookurl, content=f"{ping}")
 
 def Webhook(title):
-    embed = DiscordEmbed(title=f"{title}", color=0x000000)
-    webhook.add_embed(embed)
-    webhook.execute()
-    webhook.remove_embed(0)
+    if webhook_enabled == True:
+        embed = DiscordEmbed(title=f"{title}", color=0x000000)
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.remove_embed(0)
     subprocess.call("taskkill /f /im RobloxPlayerBeta.exe", shell=True)
 
 def Check():
     ScreenShot = pyautogui.screenshot(region=(700, 200, 550, 550))
-    ScreenShot.save(r'Images/Drops.png')
-    Drops = Image.open('Images/Drops.png')
+    ScreenShot.save(r'Drops.png')
+    Drops = Image.open('Drops.png')
     DroppedItems = pytesseract.image_to_string(Drops)
     if check_hog == True:
         if "Hogyoku Fragment" in DroppedItems:
@@ -38,12 +44,13 @@ def Check():
 while True:
     win = win32gui.FindWindow(None, "Roblox")
     if win:
-        if pyautogui.locateOnScreen('Images/JoinLobby.png'):
-            x, y = pyautogui.locateCenterOnScreen('Images/JoinLobby.png')
-            autoit.mouse_click("", x, y)
+        time.sleep(sleep_timer)
+        if pyautogui.locateOnScreen('JoinLobby.png'):
+            x, y = pyautogui.locateCenterOnScreen('JoinLobby.png')
+            autoit.MouseClick("", x, y)
         s = pyautogui.screenshot()
         for x in range(s.width):
             for y in range(s.height):
                 if s.getpixel((x, y)) == (58, 244, 64):
-                    autoit.mouse_click("", x, y)
-    Check()
+                    autoit.MouseClick("", x, y)
+        Check()
